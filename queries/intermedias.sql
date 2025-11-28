d58c8b42de821cc4c89143ab467ab0dfd685af5a
Julia Sidichenko <iuliasidichenko@gmail.com>
2025-11-28 11:48:25 +0100

    Add query for clients born after 1990
    Added a query to list clients born after 1990 and their classes.

--- queries/intermedias.sql
+++ queries/intermedias.sql
@@ -12,3 +12,12 @@ SELECT c.nombre, c.apellido
 FROM clientes c
 LEFT JOIN inscripciones i USING (cliente_id)
 WHERE i.cliente_id IS NULL;
+
+
+-- Listar clientes nacidos después de 1990 y a qué clases están apuntados.
+
+select cli.nombre, cli.apellido, cli.fecha_nacimiento, c.nombre_clase
+from inscripciones
+join clases c using (clase_id)
+join clientes cli using (cliente_id)
+where YEAR(fecha_nacimiento) > 1990;
