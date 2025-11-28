5b655a9c4859dd9d2e5e65cec6b71706e13d8de4
Julia Sidichenko <iuliasidichenko@gmail.com>
2025-11-28 12:40:56 +0100

    Update and rename avanzadas to avanzadas.sql

---
+++ queries/avanzadas.sql
@@ -0,0 +1,59 @@
+-- Antes de insertar en inscripciones, revisa si ese cliente ya está inscrito a esa clase.
+
+DELIMITER $$
+
+CREATE TRIGGER tr_prevenir_duplicados
+BEFORE INSERT ON inscripciones
+FOR EACH ROW
+BEGIN
+	IF EXISTS 	   ( SELECT 1
+                     FROM inscripciones
+                     WHERE cliente_id = new.cliente_id
+                     AND clase_id = NEW.clase_id ) then
+                     
+	SIGNAL SQLSTATE '45000'
+	SET MESSAGE_text = 'Este cliente ya esta inscrito';
+        
+	END IF;
+    END$$
+
+-- Obtener la edad de un cliente
+      
+DELIMITER $$
+
+CREATE FUNCTION f_edad_cliente (p_cliente_id INT)
+RETURNS INT
+DETERMINISTIC
+BEGIN 
+DECLARE v_edad INT;
+
+       SELECT TIMESTAMPDIFF(YEAR, fecha_nacimiento, CURDATE()) into v_edad
+       FROM clientes
+       WHERE cliente_id = p_cliente_id;
+       
+RETURN v_edad ;
+END $$
+DELIMITER ;
+
+select f_edad_cliente(5)
+
+-- total pagado por un cliente
+  
+DELIMITER $$
+
+CREATE FUNCTION fn_total_pagado(p_cliente_id INT)
+RETURNS DECIMAL(10,2)
+DETERMINISTIC
+BEGIN
+DECLARE v_total DECIMAL(10,2);
+			
+            SELECT coalesce((monto),0) into v_total
+            FROM pagos
+            where cliente_id = p_cliente_id;        
+           
+RETURN v_total ;
+END $$
+DELIMITER ;
+
+
+select fn_total_pagado(8)
