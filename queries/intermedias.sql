c63460d713c1e856fada39efeb68826cce32fd0d
Julia Sidichenko <iuliasidichenko@gmail.com>
2025-11-28 11:47:54 +0100

    Update intermedias.sql

--- queries/intermedias.sql
+++ queries/intermedias.sql
@@ -1,1 +1,14 @@
+-- Mostrar los monitores que NO tienen clases asignadas.
 
+select m.nombre, m.apellido
+from monitores m
+WHERE m.monitor_id NOT IN (
+    SELECT DISTINCT monitor_id FROM clases WHERE monitor_id IS NOT NULL
+);
+
+-- Ver los clientes que NO tienen inscripciones.
+
+SELECT c.nombre, c.apellido
+FROM clientes c
+LEFT JOIN inscripciones i USING (cliente_id)
+WHERE i.cliente_id IS NULL;
